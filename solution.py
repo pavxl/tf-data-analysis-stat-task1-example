a241feb5c02d24962c8627bd884b67bfa2168c0c
pavxl <76624639+pavxl@users.noreply.github.com>
2023-04-10 23:54:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,4 @@ import numpy as np
 chat_id = 362844815
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return x.mean()/54
